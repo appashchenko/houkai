800538445b635cad45ee0976ec931ae5e55b7dbc
Zetrin0 <pashchenko.a.o@gmail.com>
2021-12-22 18:02:30 +0600

    Revert "gtk initial"
    This reverts commit 685826d0bed8b4d11070f408d958f0d7376ef311.

--- src/main-gtk.c
+++ src/main-gtk.c
@@ -1,5 +1,7 @@
 #include <gtk/gtk.h>
 
+GtkWidget *main_window;
+
 enum { LABEL_COLUMN = 0, ID_COLUMN, OFFSET_COLUMN, N_COLUMN };
 
 static void app_quit_activated(GSimpleAction *action, GVariant *parameter,
@@ -7,18 +9,17 @@ static void app_quit_activated(GSimpleAction *action, GVariant *parameter,
   g_application_quit(G_APPLICATION(app));
 }
 
-static void on_app_activate(GtkApplication *app, gpointer user_data);
+static void app_activate(GtkApplication *app, gpointer user_data);
+static void open_file_dialog_response(GtkDialog *, GtkResponseType);
 static void open_file_dialog_activated(GSimpleAction *, GVariant *, gpointer);
 GtkTreeModel *fill_store(void);
-
 
 int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app =
-      gtk_application_new("org.houkai.AudioExplorer", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(on_app_activate), NULL);
+  app = gtk_application_new("org.houkai.explorer", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
@@ -45,9 +46,10 @@ GtkTreeModel *fill_store(void) {
   return GTK_TREE_MODEL(store);
 }
 
-static void on_app_activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *main_window;
+static void app_activate(GtkApplication *app, gpointer user_data) {
+  // GtkWidget *main_window;
   GtkWidget *main_box;
+  GtkWidget *tool_box;
   GtkWidget *nav;
   GtkWidget *items;
   GtkTreeModel *model;
@@ -65,15 +67,15 @@ static void on_app_activate(GtkApplication *app, gpointer user_data) {
   GSimpleAction *act_open = g_simple_action_new("open", NULL);
   g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(act_open));
   g_signal_connect(act_open, "activate", G_CALLBACK(open_file_dialog_activated),
-                   main_window);
+                   app);
 
   GMenu *menubar = g_menu_new();
-  GMenuItem *menu_item_file = g_menu_item_new("_File", NULL);
+  GMenuItem *menu_item_file = g_menu_item_new("File", NULL);
   GMenu *menu_file = g_menu_new();
-  GMenuItem *menu_file_open = g_menu_item_new("_Open", "app.open");
+  GMenuItem *menu_file_open = g_menu_item_new("Open", "app.open");
   g_menu_append_item(menu_file, menu_file_open);
   g_object_unref(menu_file_open);
-  GMenuItem *menu_file_exit = g_menu_item_new("_Exit", "app.quit");
+  GMenuItem *menu_file_exit = g_menu_item_new("Exit", "app.quit");
   g_menu_append_item(menu_file, menu_file_exit);
   g_object_unref(menu_file_exit);
 
@@ -84,15 +86,24 @@ static void on_app_activate(GtkApplication *app, gpointer user_data) {
   gtk_application_window_set_show_menubar(GTK_APPLICATION_WINDOW(main_window),
                                           TRUE);
 
+  tool_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  GtkWidget *button = gtk_button_new_with_label("Play");
+  gtk_box_append(GTK_BOX(tool_box), button);
+  GtkWidget *sep1 = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_box_append(GTK_BOX(tool_box), sep1);
+  gtk_window_set_child(GTK_WINDOW(main_window), tool_box);
+
   main_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_window_set_child(GTK_WINDOW(main_window), main_box);
 
-  model = fill_store();
+  // model = fill_store();
 
-  nav = gtk_tree_view_new_with_model(model);
+  // nav = gtk_tree_view_new_with_model(model);
+  nav = gtk_tree_view_new();
   gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(nav), FALSE);
+  gtk_widget_set_hexpand(nav, TRUE);
 
-  g_object_unref(G_OBJECT(model));
-
+  // g_object_unref(G_OBJECT(model));
   renderer = gtk_cell_renderer_text_new();
   column = gtk_tree_view_column_new_with_attributes(NULL, renderer, "text",
                                                     LABEL_COLUMN, NULL);
@@ -100,57 +111,52 @@ static void on_app_activate(GtkApplication *app, gpointer user_data) {
   g_signal_connect(nav, "realize", G_CALLBACK(gtk_tree_view_expand_all), NULL);
 
   items = gtk_tree_view_new();
-  gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(items), TRUE);
+  gtk_widget_set_hexpand(items, TRUE);
 
-  gtk_box_pack_start(GTK_BOX(main_box), nav, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(main_box), items, TRUE, TRUE, 0);
-
-  gtk_container_add(GTK_CONTAINER(main_window), main_box);
-
-  gtk_widget_show_all(main_window);
+  gtk_box_append(GTK_BOX(main_box), nav);
+  gtk_box_append(GTK_BOX(main_box), items);
 
   gtk_window_present(GTK_WINDOW(main_window));
 }
 
-/*static void open_file_dialog_response(GtkDialog *dialog,
+static void open_file_dialog_response(GtkDialog *dialog,
                                       GtkResponseType response) {
-  GSList *files = NULL;
-  GSList *item = NULL;
+  GListModel *files = NULL;
 
   if (response == GTK_RESPONSE_ACCEPT) {
     GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
     files = gtk_file_chooser_get_files(chooser);
 
-    item = files;
-
-    while (item != NULL) {
+    for (guint i = 0;; i++) {
       char *filename = NULL;
       GFile *file = NULL;
 
-      file = item->data;
+      file = g_list_model_get_item(files, i);
+      if (file == NULL)
+        break;
 
       filename = g_file_get_path(file);
       g_message("%s\n", filename);
       g_free(filename);
       g_object_unref(file);
-
-      item = g_slist_next(item);
     }
 
     g_object_unref(files);
   }
-}*/
+
+  gtk_window_destroy(GTK_WINDOW(dialog));
+}
 
 static void open_file_dialog_activated(GSimpleAction *action,
-                                       GVariant *parameter, gpointer parent) {
+                                       GVariant *parameter, gpointer app) {
   GtkWidget *dialog;
 
   GtkFileChooserAction action_type = GTK_FILE_CHOOSER_ACTION_OPEN;
   GtkFileFilter *filter;
 
   dialog = gtk_file_chooser_dialog_new(
-      "Open File", GTK_WINDOW(parent), action_type, "_Cancel",
-      GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
+      "Open File", GTK_WINDOW(main_window), action_type, "Cancel",
+      GTK_RESPONSE_CANCEL, "Open", GTK_RESPONSE_ACCEPT, NULL);
 
   filter = gtk_file_filter_new();
   gtk_file_filter_set_name(filter, "Wwise AKPK Storage (PCK)");
@@ -160,13 +166,9 @@ static void open_file_dialog_activated(GSimpleAction *action,
   gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(dialog), TRUE);
   gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT);
 
-  if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-    char *filename;
-    filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-    g_free(filename);
-  }
-
-  gtk_widget_destroy(dialog);
+  gtk_widget_show(dialog);
+  g_signal_connect(dialog, "response", G_CALLBACK(open_file_dialog_response),
+                   NULL);
 }
 
 /* vim: ts=2 sw=2 expandtab
